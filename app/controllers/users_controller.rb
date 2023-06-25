b62a2b80a9244f3d5a73606198dfac4f8df3b50e
ShoheiyoShida <129791157+49822@users.noreply.github.com>
2023-06-25 10:29:27 +0000

    制作9’ git commit -m  制作10’

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -14,6 +15,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -22,6 +24,9 @@ class UsersController < ApplicationController
     else
       render "edit"
     end
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
   end
 
   private
@@ -29,6 +34,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
+
+   def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+   end
 
   def ensure_correct_user
     @user = User.find(params[:id])
